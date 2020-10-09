2c0cbccf08e43cb827789513bb5ef8177847a172
yonatanmk <yonatanmk@gmail.com>
2020-10-09 15:56:27 -0400

    button color

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SketchPicker } from 'react-color';
+import { getContrast } from './utils';
 
 class ColorPicker extends React.Component {
   state = {
@@ -11,9 +12,14 @@ class ColorPicker extends React.Component {
   render() {
     const {color, handleColorChange} = this.props;
     const { display } = this.state;
+    const contrastColor = getContrast(color);
     return (
       <div className="colorpicker">
-        <button className="colorpicker-button" onClick={this.toggleDisplay}>{display ? 'Hide' : color.toUpperCase() }</button>
+        <button 
+          className="colorpicker-button"
+          style={{ backgroundColor: color, color: contrastColor, border: `1px solid ${contrastColor === 'white' ? color : contrastColor}`}}
+          onClick={this.toggleDisplay}>{display ? 'Hide' : color.toUpperCase() }
+        </button>
         {display && <div className="colorpicker-container">
           <SketchPicker
             color={ color }
